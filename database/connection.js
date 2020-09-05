11a358bacee72f7438ad725f88709a467dc6d0a6
Eleson Souza <elesonsouza17@hotmail.com>
2020-09-05 20:10:52 -0300

    Atualizacao da conexao com o banco de dados para a RDS da AWS

--- database/connection.js
+++ database/connection.js
@@ -1,9 +1,9 @@
 var knex = require('knex')({
     client: 'mysql2',
     connection: {
-        host : '127.0.0.1',
-        user : 'root',
-        password : '',
+        host : 'ls-f6d2e61fcbc5a535aa71d1aaf005fcfcce05c5e4.crlizib8w9lo.us-east-1.rds.amazonaws.com',
+        user : 'dbmasteruser',
+        password : '6{O+q3-#<4FftK)kS6Ac?r_s5R;Nz,Dk',
         database : 'db_forum_devs'
     }
 });
